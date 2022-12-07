392aa9fc0879fd8c17a82eeb57096dcc00c5759e
eliasjoostens <59340987+eliasjoostens@users.noreply.github.com>
2022-12-07 08:52:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/breukentrainer2/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.breukentrainer2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnStartBasic = findViewById<Button>(R.id.buttonBasic)
+        val btnStartAdvanced = findViewById<Button>(R.id.buttonAdvanced)
+
+        btnStartBasic.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            intent.putExtra("Level","Basic")
+            // start your next activity
+            startActivity(intent)
+
+            finish()
+        }
+
+        btnStartAdvanced.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            intent.putExtra("Level","Advanced")
+            // start your next activity
+            startActivity(intent)
+
+            finish()
+        }
+    }
+}
